1f45ef8f198fbaa05515a098910b5858c5878bb6
andrew672 <amarbach5@gmail.com>
2026-01-20 19:37:01 +0100

    init project

---
+++ src/main/java/fr/cambouiscorp/jobmailer/JobMailer/JobMailerApplication.java
@@ -0,0 +1,13 @@
+package fr.cambouiscorp.jobmailer.JobMailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMailerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/cambouiscorp/jobmailer/JobMailer/JobMailerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cambouiscorp.jobmailer.JobMailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
